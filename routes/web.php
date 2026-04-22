02fcbf9970713539d2c1c674e4e77aebc27066d8
Jeff Yarnall <jyarnall@imaginesocialstudios.com>
2026-04-22 11:59:00 -0500

    updated the webinar routes file to include the webhooks for Zoom

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Webhooks\ZoomWebhookController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth');
-
-Route::post('/webhooks/zoom', ZoomWebhookController::class)
-    ->name('webhooks.zoom');
--- routes/webinar.php
+++ routes/webinar.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\Public\WebinarJoinRedirectController;
 use App\Http\Controllers\Public\WebinarRegistrationController;
+use App\Http\Controllers\Webhooks\ZoomWebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebinarRegistrationController::class, 'index'])
     ->name('webinar.index');
+
+Route::post('/webhooks/zoom', ZoomWebhookController::class)
+    ->name('webhooks.zoom');
 
 Route::get('/j/{token}', WebinarJoinRedirectController::class)
     ->name('webinar.join.redirect');
